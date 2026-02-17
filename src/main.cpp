7127b2cb44e139d315cc8722ddd18b507d9b468b
deepak chauhan <deepak.kumar_cs.aiml24@gla.ac.in>
2026-02-17 09:08:08 +0530

    wrote the code of void setup() and void loop()

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,53 @@
+// #include <Arduino.h>
+// #include <DHT.h>
+// #define DHTPIN 2
+// #define DHTTYPE DHT11
+// DHT dht(DHTPIN , DHTTYPE);
+// void setup() {
+// // write your initialization code here
+//     dht.begin();
+//     Serial.println("DHT11 sensor initialized");
+// }
+//
+// void loop() {
+// // write your code here
+//     float humidity = dht.readHumidity();
+//     float temperature = dht.readTemperature();
+//     if (isnan(humidity) || isnan(temperature)) {
+//         Serial.println("Failed to read from sensor!");
+//         delay(2000);
+//         return;
+//     }
+//     Serial.print("Humidity");
+//     Serial.println(humidity);
+//     Serial.print(" % | ");
+//     Serial.print("Temperature");
+//     Serial.println(temperature);
+//     Serial.println(" C");
+//     delay(2000);
+// }
 #include <Arduino.h>
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
-// write your initialization code here
+    dht.begin();
+    Serial.println("DHT11 sensor initialized");
 }
 
 void loop() {
-// write your code here
+    float humidity =dht.readHumidity();
+    float temperature =dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+    delay(2000);
 }
